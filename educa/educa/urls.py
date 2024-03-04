b3609559f091c2231d098045a8048420369ceaa3
vincenttommi <vincenttommi@gmail.com>
2024-03-04 13:19:22 +0300

    running migrations

---
+++ educa/educa/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import path
+from django.conf  import settings
+from django.conf.urls.static  import static
+
+
+
+urlpatterns = [
+    
+    path('admin/', admin.site.urls),
+    
+]
+
+if settings.DEBG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    
+#a helper function to serve  media files with Django development server during development
+#suitable for development but not production use 
