df31806f266c6088e156925d4174522715650185
vincenttommi <vincenttommi@gmail.com>
2024-03-06 17:11:53 +0300

    creating list template for courses

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf  import settings
 from django.conf.urls.static  import static
 from django.contrib.auth import views as auth_views
@@ -12,6 +12,7 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     #django authetication framework for users to authenticate
     path('accounts/logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('course/', include('courses.urls')),
     
     path('admin/', admin.site.urls),
     
